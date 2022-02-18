729f951e12e25fb88995c7868a0edfab90384ae8
Shailja Kant Tiwari <smartshailjakant1998@gmail.com>
2022-02-18 22:38:37 +0530

    copy.py files helps us in copying files parallely using different threads

---
+++ copy.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import subprocess
+import os
+import sys
+from concurrent import futures
+
+
+path=sys.argv[1]
+
+list = os.listdir()
+
+def copy(i,path):
+	subprocess.run(['cp','-rv',i,path])
+
+
+executor = futures.ThreadPoolExecutor()
+for i in list:
+	executor.submit(copy,i,path)  #executor will run all the task in parallel
+
+print("waiting for all files to copy")  #loop will be finished as soon as all the task are scheduled hence writing this message
+executor.shutdown()
